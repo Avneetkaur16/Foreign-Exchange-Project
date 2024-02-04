37eefc8006ec32f6ae8ccd2a832892f0a81a1666
Avneet Kaur <63954648+Avneetkaur16@users.noreply.github.com>
2024-02-04 01:17:08 -0600

    Update Timeframe.jsx

--- src/pages/timeframe/Timeframe.jsx
+++ src/pages/timeframe/Timeframe.jsx
@@ -61,7 +61,7 @@ const Timeframe = () => {
                 <p>Select To Convert: </p>
                 <select onChange={handleSelect}>
                     <option defaultValue='All'>All</option>
-                    {Object.keys(symbols).map(key => (
+                    {symbols && Object.keys(symbols).map(key => (
                         <option value={key}>{key}: {symbols[key]}</option>
                     ))}
                 </select>
